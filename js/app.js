db3990d7f0b524cf857a96f3e2508a251500f097
Alfredo Miranda <alfredo@miranda.io>
2014-10-04 22:15:52 -0500

    Fix fade in/out animation

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 $(function() {
-  var questions = [];
+  var questions = [{
+    'question': 'What is printed in the console?',
+    'code': 'var foo = function foo() {\nconsole.log(foo == foo);\n};\nfoo();',
+    'choices': ['true', 'false', 'ReferenceError'],
+    'answer': 'true'
+  }, {
+    'question': 'What does the following snippet of code alert?'
+    'code': 'function aaa() {\nreturn\n{\ntest: 1\n};\n}\nalert(typeof aaa());',
+    'answer': ];
   $('.start').click(function() {
-    $('.welcome').fadeOut(function() {
-      $('.quiz').fadeIn();
+    $('.welcome').fadeOut('fast', function() {
+      $('.quiz').fadeIn('fast');
     });
   });
 });
