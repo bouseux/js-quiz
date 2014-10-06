731b4bbb68dc4e083474ae6977d96dc14f1e3058
Alfredo Miranda <alfredo@miranda.io>
2014-10-05 21:36:17 -0500

    First question is displayed

--- js/app.js
+++ js/app.js
@@ -1,14 +1,35 @@
 $(function() {
+  var current = 0;
   var questions = [{
     'question': 'What is printed in the console?',
-    'code': 'var foo = function foo() {\nconsole.log(foo == foo);\n};\nfoo();',
+    'code': 'code1.html',
     'choices': ['true', 'false', 'ReferenceError'],
     'answer': 'true'
   }, {
-    'question': 'What does the following snippet of code alert?'
-    'code': 'function aaa() {\nreturn\n{\ntest: 1\n};\n}\nalert(typeof aaa());',
-    'answer': ];
+    'question': 'What does the following snippet of code alert?',
+    'code': 'code2.html',
+    'choices': ['function', 'number', 'object', 'undefined'],
+    'answer': 'undefined'
+  }];
+
+  var setQuestion = function() {
+    var question = questions[current];
+    var numChoices = question.choices.length;
+    var ul = $('ul');
+
+    $('.question').text(question.question);
+    $.get('views/' + question.code, function(data) {
+      console.log('views/' + question.code);
+      $('.code').text(data);
+    });
+
+    for(var i = 0; i < numChoices; i++) {
+      ul.append('<li>' + question.choices[i] + '</li>');
+    }
+  };
+
   $('.start').click(function() {
+    setQuestion();
     $('.welcome').fadeOut('fast', function() {
       $('.quiz').fadeIn('fast');
     });
