fac8e465d0617c595a698ec352b3fe92dbd83e1a
Alfredo Miranda <alfredo@miranda.io>
2014-10-06 23:24:59 -0500

    Add more questions

--- js/app.js
+++ js/app.js
@@ -10,6 +10,31 @@ $(function() {
     'code': 'code2.html',
     'choices': ['function', 'number', 'object', 'undefined'],
     'answer': 'undefined'
+  }, {
+    'question': 'What is the result?',
+    'code': 'code3.html',
+    'choices': ['true', 'false', 'TypeError'],
+    'answer': 'true';
+  }, {
+    'question': 'What is the result?',
+    'code': 'code4.html',
+    'choices': ['true', 'false', 'TypeError', 'NaN'],
+    'answer': 'false'
+  }, {
+    'question': 'What is alerted?',
+    'code': 'code5.html',
+    'choices': ['number', 'function', 'undefined', 'string', 'Error'],
+    'answer': 'function'
+  }, {
+    'question': 'What is the result?',
+    'code': 'code6.html',
+    'choices': ['0', '2', '11', '"11"'],
+    'answer': '2'
+  }, {
+    'question': 'What is the order of values alerted?',
+    'code': 'code7.html',
+    'choices': ['1, 2', '1, 3', '2, 1', '2, 3', '3, 1', '3, 2'],
+    'answer': '3, 1'
   }];
 
   var setQuestion = function() {
